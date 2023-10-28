30ed5c0a341bf7e0347e29a0df2ab993642cb161
radwan503 <radwancse503@gmail.com>
2023-10-29 01:30:29 +0600

    dev content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { SaleComponent } from './pages/sale/sale.component';
 
 const routes: Routes = [
-  {
-    path: "products",
-    component: HomeComponent
-  },
-  {
-    path: "cart",
-    component: CartComponent
-  },
-  {
-    path: "sale",
-    component: SaleComponent
-  }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { SaleComponent } from './pages/sale/sale.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CartComponent,
-    SaleComponent
   ],
   imports: [
     BrowserModule,
